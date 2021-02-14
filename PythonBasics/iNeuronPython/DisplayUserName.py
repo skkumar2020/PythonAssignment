abc4c164c17664458eac6e209c57010d526c6bb4
Shreekrishna Kumar D <skkumar2020@gmail.com>
2021-02-14 14:19:41 +0530

    adding Get & print username code

---
+++ PythonBasics/iNeuronPython/DisplayUserName.py
@@ -0,0 +1,6 @@
+#code to get user's first & last name and display
+#@author Shreekrishna
+
+first_name = input("Enter your FirstName: ") 
+last_name = input("Enter your LastName: ") 
+print(last_name," ",first_name)
