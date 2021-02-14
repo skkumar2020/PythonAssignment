095301194f96e67bee712af5a8a1f95b484e97f2
Shreekrishna Kumar D <skkumar2020@gmail.com>
2021-02-14 14:30:11 +0530

    added CalculateVolumeOfSphere.py

---
+++ PythonBasics/iNeuronPython/CalculateVolumeOfSphere.py
@@ -0,0 +1,8 @@
+#Write a Python program to find the volume of a sphere with diameter 12 cm.
+#author Shreekrishna
+
+pi = 3.1415926535897931
+d = 21.0
+r = d/2 
+V= 4.0/3.0*pi* r**3
+print('The volume of the sphere is: ',V)
